0fd0b7f034861690c081a754cdd638093cec30bf
mausamazing <mausamkaushal108@gmail.com>
2024-07-08 16:30:38 +0530

    fixed a big

--- __main__.py
+++ __main__.py
@@ -1,14 +1,24 @@
-import wave, time,json
 from lyrics import get_lyrics
 from fingerprinting import acoust_fingerprint_generator
 from API import request_handler
 from listener import audio_listener
+import wave, time,json
 
-
-data = json.load("song_data.json")
 listen_in_real_time = False
 last_fingerprint =""
 current_lyrics = ""
+
+import json
+
+# Read the content of the file
+def read():
+    global data
+    with open("song_data.json", "r") as file:
+        content = file.read()
+        data = json.loads(content)
+read()
+old_song_name = data["name"]
+old_author_name = data["artist"]
 
 def get_duration_wave(file_path):
    with wave.open(file_path, 'r') as audio_file:
@@ -16,47 +26,73 @@ def get_duration_wave(file_path):
       n_frames = audio_file.getnframes()
       duration = n_frames / float(frame_rate)
       return duration
+def old_code():#    might need later
 
+    # if __name__ == "__main__":
 
-if __name__ == "__main__":
+    #     if listen_in_real_time:
+    #         audio_file = 'output.wav'
+    #         try:
+    #             while True:
+    #                 audio_listener.listen()
+    #                 time.sleep(6)
+    #                 duration = get_duration_wave(audio_file)
+    #                 print(f"Duration: {duration:.2f} seconds")
+    #                 fingerprint = acoust_fingerprint_generator.generate_audio_fingerprint(audio_file)
+    #                 print("Generated fingerprint...")
+    #                 if last_fingerprint != fingerprint:
+    #                     last_fingerprint = fingerprint
+    #                     save_fingerprint = open("fingerprint.txt","w")
+    #                     save_fingerprint.write(str(fingerprint))
+    #                     request_handler.lookup_acoustid(fingerprint=fingerprint,duration=duration)
+    #                     try:
+    #                         song = request_handler.song_name
+    #                         author = request_handler.artist_name
+    #                         current_lyrics = get_lyrics.get_data(song,author_name=author)
+    #                     except AttributeError:
+    #                         print("No results found")
+    #                 else:
+    #                     song = request_handler.song_name 
+    #                     author = request_handler.artist_name
+    #                     current_lyrics = get_lyrics.get_data(song,author_name=author)
+                    
+    #         except KeyboardInterrupt:
+    #             print("Closing the program")
+    #     else:
+    #         audio_file = 'old_output.wav'
+    #         duration = get_duration_wave(audio_file)
+    #         print(f"Duration: {duration:.2f} seconds")
+    #         fingerprint = acoust_fingerprint_generator.generate_audio_fingerprint(audio_file)
+    #         print("Generated fingerprint...")
+    #         if last_fingerprint != fingerprint:
+    #             last_fingerprint = fingerprint
+    #             request_handler.lookup_acoustid(fingerprint=fingerprint,duration=duration)
+    #             song = request_handler.song_name 
+    #             author = request_handler.artist_name
+    #             current_lyrics = get_lyrics.get_data(song,author_name=author)
+    return
 
-    if listen_in_real_time:
-        audio_file = 'output.wav'
+def write():
+    writer = open("lyrics.txt","w")
+    writer.write(get_lyrics.final_lyrics)
+if __name__=="__main__":
+    read()
+    lyrics = get_lyrics.get_data(song_name=old_song_name,author_name=old_author_name)
+    write()
+    while True:
+        time.sleep(3)
         try:
-            while True:
-                audio_listener.listen()
-                time.sleep(6)
-                duration = get_duration_wave(audio_file)
-                print(f"Duration: {duration:.2f} seconds")
-                fingerprint = acoust_fingerprint_generator.generate_audio_fingerprint(audio_file)
-                print("Generated fingerprint...")
-                if last_fingerprint != fingerprint:
-                    last_fingerprint = fingerprint
-                    save_fingerprint = open("fingerprint.txt","w")
-                    save_fingerprint.write(str(fingerprint))
-                    request_handler.lookup_acoustid(fingerprint=fingerprint,duration=duration)
-                    try:
-                        song = request_handler.song_name
-                        author = request_handler.artist_name
-                        current_lyrics = get_lyrics.get_data(song,author_name=author)
-                    except AttributeError:
-                        print("No results found")
-                else:
-                    song = request_handler.song_name 
-                    author = request_handler.artist_name
-                    current_lyrics = get_lyrics.get_data(song,author_name=author)
-                
-        except KeyboardInterrupt:
-            print("Closing the program")
-    else:
-        audio_file = 'old_output.wav'
-        duration = get_duration_wave(audio_file)
-        print(f"Duration: {duration:.2f} seconds")
-        fingerprint = acoust_fingerprint_generator.generate_audio_fingerprint(audio_file)
-        print("Generated fingerprint...")
-        if last_fingerprint != fingerprint:
-            last_fingerprint = fingerprint
-            request_handler.lookup_acoustid(fingerprint=fingerprint,duration=duration)
-            song = request_handler.song_name 
-            author = request_handler.artist_name
-            current_lyrics = get_lyrics.get_data(song,author_name=author)
+            read()
+            song_name = data["name"]
+            author_name = data["artist"]
+            print(song_name,author_name)
+            if(old_author_name!=author_name or old_song_name!=song_name):
+                old_author_name = author_name
+                old_song_name = song_name
+                lyrics = get_lyrics.get_data(song_name=song_name,author_name=author_name)
+                write()
+            else:
+                # print("Lyrics not changing yet...")
+                continue
+        except Exception as e:
+            print(e)
--- lyrics/get_lyrics.py
+++ lyrics/get_lyrics.py
@@ -1,10 +1,36 @@
 from lyricsgenius import Genius
-genius = Genius("token from lyrics")
-def get_data(song_name, **author_name):
-        artist = author_name or ""
-        song_data = genius.search_song(song_name,author_name["author_name"])
-        lyrics = song_data.lyrics
-        index = lyrics.index(song_name)
 
+genius = Genius("token from lyrics")
+global is_corrupted 
+is_corrupted= False #checks if the lyrics are extra super duper corrupted
+def get_data(song_name, author_name):
+        global final_lyrics
+    
+        # Search for the song
+        song_data = genius.search_song(song_name, author_name)
+        
+        if song_data is None:
+                raise ValueError(f"No lyrics found for '{song_name}' by '{author_name}'")
+        
+        # Get the lyrics
+        lyrics = song_data.lyrics
+        
+        # Find the index of the song name in the lyrics
+        index = lyrics.find(song_name)
+        split_name = song_name.rsplit()
+        first_word = split_name[0]
+        if index == -1:
+                string = f"Contributors{first_word}".lower()
+                print(f"--------->{string, 'ContrbutorsTHE'.lower()} {string.lower()=='ContributorsTHE'.lower()}")
+
+                index = lyrics.lower().find(string)
+                print(lyrics[index+12:len(lyrics)-7])  # Adjust the slicing as needed
+                final_lyrics = lyrics[index+12:len(lyrics)-7]
+                return final_lyrics
+                if index == -1:
+                        raise ValueError(f"Cannot find Contributors'{song_name}' in the lyrics",lyrics)
+        
         print(f"{index} is the index")
-        print(lyrics[index:len(lyrics)-7])
+        print(lyrics[index:len(lyrics)-7])  # Adjust the slicing as needed
+        final_lyrics = f"{lyrics[index:len(lyrics)-7]}"
+        return final_lyrics
